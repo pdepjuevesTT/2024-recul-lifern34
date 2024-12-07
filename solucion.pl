fd4671d660faaf0aa18c55c32bc8dee231b1de8c
Lisandro Fernández <164579897+lifern34@users.noreply.github.com>
2024-12-07 09:31:50 -0300

    Primer punto y avances

--- solucion.pl
+++ solucion.pl
@@ -1,1 +1,24 @@
-% Solución en Prolog
+%% PUNTO 1)
+%% tienePropiedad(Persona,Propiedad).
+tienePropiedad(juan,casa(120)).
+tienePropiedad(nico,departamento(3,2)).
+tienePropiedad(alf,departamento(3,1)).
+tienePropiedad(julian,loft(2000)).
+tienePropiedad(vale,departamento(4,1)).
+tienePropiedad(fer,casa(110)).
+%% En el futuro ver que hacer con Rocio, se quiere mudar pero NO tiene propiedad.
+
+%% viveEn(Persona,Barrio).
+viveEn(alf,almagro).
+viveEn(juan,almagro).
+viveEn(nico,almagro).
+viveEn(julian,almagro).
+viveEn(vale,flores).
+viveEn(fer,flores).
+
+%% PUNTO 2)
+barrioCopado(Barrio):-
+    viveEn(_,Barrio),
+    forall((tienePropiedad(Persona,Propiedad),viveEn(Persona,Barrio)),esCopada(Propiedad)).
+
+
